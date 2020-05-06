dbfd1561b63edcd71c1671209ddef3e18afa0836
Endogen <081081@gmx.net>
2020-05-06 02:15:04 +0200

    Implemented request method and getting nonce & epoch

--- python/dnasend/dnaprovider.py
+++ python/dnasend/dnaprovider.py
@@ -1,4 +1,9 @@
+import json
+import requests
+
 from abc import ABC, abstractmethod
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 
 class DnaProvider(ABC):
@@ -21,25 +26,46 @@ class DnaProvider(ABC):
 
 class IdenaDevDnaProvider(DnaProvider):
 
-    def __init__(self, url: str="rpc.idena.dev"):
+    def __init__(self, url: str="rpc.idena.dev", timeout: int=5):
+        self.timeout = timeout
         self.url = url
         self.id = 0
 
-    def request(self, command: str, params: list) -> dict:
-        # TODO
-        pass
+        self.session = requests.Session()
+        retries = Retry(total=5, backoff_factor=0.5, status_forcelist=[502, 503, 504])
+        self.session.mount('http://', HTTPAdapter(max_retries=retries))
 
-    def getNonceForAddress(self, address: str) -> tuple:
-        res = self.request("dna_getBalance", [address])
-        return res["nonce"], res["balance"]
+    def request(self, payload: dict) -> dict:
+        try:
+            response = self.session.get(self.url, timeout=self.timeout, json=payload)
+            response.raise_for_status()
+            content = json.loads(response.content.decode("utf-8"))
+            return content["result"]
+        except Exception as e:
+            # check if json (with error message) is returned
+            try:
+                content = json.loads(response.content.decode("utf-8"))
+                raise ValueError(content)
+            # if no json
+            except json.decoder.JSONDecodeError:
+                pass
+            raise
 
-    def getCurrentEpoch(self) -> dict:
-        res = self.request("dna_epoch", [])
-        return res
+    def getNonceForAddress(self, address: str) -> int:
+        res = self.request({
+            "method": "dna_getBalance",
+            "params": [address],
+            "id": self.id
+        })
+        return res["nonce"]
+
+    def getCurrentEpoch(self) -> int:
+        res = self.request({
+            "method": "dna_epoch",
+            "id": self.id,
+        })
+        return res["epoch"]
 
     def sendTransaction(self, rawData: str) -> str:
         res = self.request("bcn_sendRawTx", [rawData])
         return res
-
-
-
